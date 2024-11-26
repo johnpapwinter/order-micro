41ef95eb73ccf42b55b8216563b1ee29a74c655c
johnpapwinter <johnpapahimonas@yahoo.com>
2024-11-26 15:22:06 +0200

    added mongo migrations

---
+++ logging/src/main/java/com/example/logging/migrations/V1_SeedData.java
@@ -0,0 +1,42 @@
+package com.example.logging.migrations;
+
+import com.example.logging.model.LoggedOrder;
+import io.mongock.api.annotations.ChangeUnit;
+import io.mongock.api.annotations.Execution;
+import io.mongock.api.annotations.RollbackExecution;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
+@ChangeUnit(id = "seed-logged-orders", order = "001", author = "johnpapwinter")
+public class V1_SeedData {
+
+    @Execution
+    public void changeSet(MongoTemplate mongoTemplate) {
+        List<LoggedOrder> initialOrders = Arrays.asList(
+                createLoggedOrder("XK1203", 30.2, 2, LocalDate.of(2024, 11, 20)),
+                createLoggedOrder("XK2304", 35.3, 3, LocalDate.of(2024, 11, 20))
+        );
+
+        mongoTemplate.insertAll(initialOrders);
+    }
+
+    @RollbackExecution
+    public void rollback(MongoTemplate mongoTemplate) {
+        mongoTemplate.dropCollection(LoggedOrder.class);
+    }
+
+
+    private LoggedOrder createLoggedOrder(String orderId, Double amount, Integer itemsCount, LocalDate date) {
+        LoggedOrder order = new LoggedOrder();
+        order.setOrderId(orderId);
+        order.setAmount(amount);
+        order.setItemsCount(itemsCount);
+        order.setDate(date);
+        return order;
+    }
+
+
+}
