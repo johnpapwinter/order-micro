6157d9ef03bc15518c1283bd423e87675474746a
johnpapwinter <johnpapahimonas@yahoo.com>
2024-11-26 16:33:36 +0200

    added messaging queue dependencies and image to dev compose

--- logging/src/main/java/com/example/logging/migrations/V0_SeedDevData.java
+++ logging/src/main/java/com/example/logging/migrations/V0_SeedDevData.java
@@ -4,14 +4,16 @@ import com.example.logging.model.LoggedOrder;
 import io.mongock.api.annotations.ChangeUnit;
 import io.mongock.api.annotations.Execution;
 import io.mongock.api.annotations.RollbackExecution;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 
+@Profile("dev")
 @ChangeUnit(id = "seed-logged-orders", order = "001", author = "johnpapwinter")
-public class V1_SeedData {
+public class V0_SeedDevData {
 
     @Execution
     public void changeSet(MongoTemplate mongoTemplate) {
